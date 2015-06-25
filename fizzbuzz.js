6389e6f40b0f0e24dd62c0a733d194d08bcfc7e6
Mallory Curtis <mallorycurtis@outlook.com>
2015-06-24 21:52:17 -0700

    Final JS upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,14 @@
-// Place your FizzBuzz code here. 
+// Place your FizzBuzz code here.
+
+
+for (var i=1; i < 21; i=i+1)
+{
+    if (i % 5 == 0 && i % 3 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
